5e2ab1535d745ff366a40d6aff8d0d456fa00318
128jazi-sys <128jaziz@frhsd.com>
2026-09-15 13:26:56 -0400

    create class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
+
+    public class HomeworkTracker {
+        private String name;
+        private int total;
+        private int completed;
+
+        public HomeworkTracker(String name, int total, int completed) {
+            this.name = name;
+            this.total = total;
+            this.completed = completed;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
+
